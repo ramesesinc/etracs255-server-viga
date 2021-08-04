88e12391403977d8c2ab7d5e2032d750caee0595
Jessie Zamora <jzamss@gmail.com>
2021-08-04 16:15:16 +0800

    LANDTAX: share: handle compromise

--- services/modules/rptis/rameses-gov-etracs-rptis-landtax-services.jar/sql/landtax_lgu_share_posting.sql
+++ services/modules/rptis/rameses-gov-etracs-rptis-landtax-services.jar/sql/landtax_lgu_share_posting.sql
@@ -55,6 +55,38 @@ select
 	rp.objid as paymentid,
 	rpi.revtype,
 	rpi.revperiod,
+	null as year, 
+	sum(rpi.amount) as amount,
+	sum(rpi.interest) as interest,
+	sum(rpi.discount) as discount 
+from rptpayment rp 
+inner join rptpayment_item rpi on rp.objid = rpi.parentid 
+inner join rptcompromise rc on rp.refid = rc.objid 
+inner join rptledger rl on rc.rptledgerid = rl.objid 
+left join sys_org o on rl.lguid = o.objid 
+where rp.refid = $P{objid}
+and rp.receiptid = $P{receiptid}
+and rpi.revperiod <> 'advance'
+and rpi.amount > 0
+group by 
+	rl.lguid,
+	o.parent_objid,
+	rl.barangayid,
+	rp.receiptid,
+	rp.objid,
+	rpi.revtype,
+	rpi.revperiod
+
+union all 	
+
+select 
+	rl.lguid, 
+	o.parent_objid as provid, 
+	rl.barangayid,
+	rp.receiptid,
+	rp.objid as paymentid,
+	rpi.revtype,
+	rpi.revperiod,
 	rpi.year, 
 	sum(rpi.amount) as amount,
 	sum(rpi.interest) as interest,
@@ -76,6 +108,40 @@ group by
 	rpi.revtype,
 	rpi.revperiod,
 	rpi.year
+
+union all 
+
+select 
+	rl.lguid, 
+	o.parent_objid as provid, 
+	rl.barangayid,
+	rp.receiptid,
+	rp.objid as paymentid,
+	rpi.revtype,
+	rpi.revperiod,
+	rpi.year, 
+	sum(rpi.amount) as amount,
+	sum(rpi.interest) as interest,
+	sum(rpi.discount) as discount 
+from rptpayment rp 
+inner join rptpayment_item rpi on rp.objid = rpi.parentid 
+inner join rptcompromise rc on rp.refid = rc.objid 
+inner join rptledger rl on rc.rptledgerid = rl.objid 
+left join sys_org o on rl.lguid = o.objid 
+where rp.refid = $P{objid}
+and rp.receiptid = $P{receiptid}
+and rpi.revperiod = 'advance'
+and rpi.amount > 0
+group by 
+	rl.lguid,
+	o.parent_objid,
+	rl.barangayid,
+	rp.receiptid,
+	rp.objid,
+	rpi.revtype,
+	rpi.revperiod,
+	rpi.year
+
 
 
 
